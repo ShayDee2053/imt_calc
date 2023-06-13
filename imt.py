24bebaf04912a0c5d4f305c31aabdeec652a90fb
Shady2053 <sashasid114@gmial.com>
2023-06-13 23:02:50 +0300

    code styling

--- imt.py
+++ imt.py
@@ -9,8 +9,10 @@ def calculate_imt():
         h = int(tbl_h.get()) / 100
 
     except ValueError:
-        messagebox.showerror(title="Error",
-                             message="Received a string, but excepted a number")
+        messagebox.showerror(
+            title="Error",
+            message="Received a string, but excepted a number"
+        )
 
     else:
 
@@ -18,42 +20,60 @@ def calculate_imt():
             m = float(tbl_m.get())
 
         except ValueError:
-            messagebox.showerror(title="Error",
-                                 message="Received a string, but excepted a number")
+            messagebox.showerror(
+                title="Error",
+                message="Received a string, but excepted a number"
+            )
 
         else:
 
             if (h <= 0) or (m <= 0):
-                messagebox.showerror(title="Result",
-                                     message="Value is less than zero")
+                messagebox.showerror(
+                    title="Result",
+                    message="Value is less than zero"
+                )
 
             elif imt <= 16:
-                messagebox.showinfo(title="Result",
-                                    message=f"BMI = {imt}. Pronounced lack of body weight")
+                messagebox.showinfo(
+                    title="Result",
+                    message=f"BMI = {imt}. Pronounced lack of body weight"
+                )
 
             elif (imt > 16) and (imt <= 18.5):
-                messagebox.showinfo(title="Result",
-                                    message=f"BMI = {imt}. Body weight deficit")
+                messagebox.showinfo(
+                    title="Result",
+                    message=f"BMI = {imt}. Body weight deficit"
+                )
 
             elif (imt > 18.5) and (imt <= 25):
-                messagebox.showinfo(title="Result",
-                                    message=f"BMI = {imt}. Normal weight")
+                messagebox.showinfo(
+                    title="Result",
+                    message=f"BMI = {imt}. Normal weight"
+                )
 
             elif (imt > 25) and (imt <= 30):
-                messagebox.showinfo(title="Result",
-                                    message=f"BMI = {imt}. Overweight")
+                messagebox.showinfo(
+                    title="Result",
+                    message=f"BMI = {imt}. Overweight"
+                )
 
             elif (imt > 30) and (imt <= 35):
-                messagebox.showinfo(title="Result",
-                                    message=f"BMI = {imt}. Obesity of the first degree")
+                messagebox.showinfo(
+                    title="Result",
+                    message=f"BMI = {imt}. Obesity of the first degree"
+                )
 
             elif (imt > 35) and (imt <= 42):
-                messagebox.showinfo(title="Result",
-                                    message=f"BMI = {imt}. Obesity of the second degree")
+                messagebox.showinfo(
+                    title="Result",
+                    message=f"BMI = {imt}. Obesity of the second degree"
+                )
 
             else:
-                messagebox.showinfo(title="Result",
-                                    message=f"BMI = {imt}. Obesity of the third degree")
+                messagebox.showinfo(
+                    title="Result",
+                    message=f"BMI = {imt}. Obesity of the third degree"
+                )
 
 
 ctk.set_appearance_mode("dark")
@@ -66,7 +86,11 @@ height = ctk.CTkLabel(
     app,
     text="Enter height: ",
     text_color="yellow",
-    font=("Times New Roman", 14, "bold")
+    font=(
+        "Times New Roman",
+        14,
+        "bold"
+    )
 )
 
 height.grid(column=0, row=0)
@@ -75,7 +99,11 @@ mass = ctk.CTkLabel(
     app,
     text="Enter mass: ",
     text_color="yellow",
-    font=("Times New Roman", 14, "bold")
+    font=(
+        "Times New Roman",
+        14,
+        "bold"
+    )
 )
 
 mass.grid(column=0, row=1)
