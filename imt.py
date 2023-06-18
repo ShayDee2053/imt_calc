6a60f90396a285f9e8c2d2c41a5e6b8e72a7f194
Shady2053 <sashasid114@gmial.com>
2023-06-19 01:38:58 +0300

    update

--- imt.py
+++ imt.py
@@ -1,23 +1,79 @@
 import tkinter
+from tkinter import *
 from tkinter import messagebox
 import customtkinter as ctk
 
 
-def calculate_imt():
+class Calculator(ctk.CTk):
 
-    try:
-        h = int(tbl_h.get()) / 100
+    def __init__(self):
 
-    except ValueError:
-        messagebox.showerror(
-            title="Error",
-            message="Received a string, but excepted a number"
+        super().__init__()
+
+        ctk.set_appearance_mode("dark")
+        self.title("BMI calculator")
+        self.geometry("300x200+650+350")
+        self.resizable(width=False, height=False)
+
+        height = ctk.CTkLabel(
+            self,
+            text="Enter height: ",
+            text_color="yellow",
+            font=(
+                "Times New Roman",
+                14,
+                "bold"
+            )
         )
 
-    else:
+        height.grid(column=0, row=0)
+
+        mass = ctk.CTkLabel(
+            self,
+            text="Enter mass: ",
+            text_color="yellow",
+            font=(
+                "Times New Roman",
+                14,
+                "bold"
+            )
+        )
+
+        mass.grid(column=0, row=1)
+
+        tbl_h = ctk.CTkEntry(
+            self,
+            border_width=1,
+            border_color="yellow",
+            width=100
+        )
+        tbl_h.grid(column=1, row=0, pady=10)
+
+        tbl_m = ctk.CTkEntry(
+            self,
+            border_width=1,
+            border_color="yellow",
+            width=100)
+        tbl_m.grid(column=1, row=1, pady=10)
+
+        btn = ctk.CTkButton(
+            self,
+            text="Calculate",
+            text_color="yellow",
+            border_width=1,
+            border_color="yellow",
+            command=lambda: self.calculate_imt(
+                int(tbl_h.get()) / 100,
+                float(tbl_m.get()))
+        )
+
+        btn.grid(column=1, row=2, pady=10)
+
+    @staticmethod
+    def calculate_imt(h, m):
 
         try:
-            m = float(tbl_m.get())
+            h
 
         except ValueError:
             messagebox.showerror(
@@ -27,116 +83,69 @@ def calculate_imt():
 
         else:
 
-            imt = m / (h**2)
-            imt = round(imt, 2)
+            try:
+                m
 
-            if (h <= 0) or (m <= 0):
+            except ValueError:
                 messagebox.showerror(
-                    title="Result",
-                    message="Value is less than zero"
-                )
-
-            elif imt <= 16:
-                messagebox.showinfo(
-                    title="Result",
-                    message=f"BMI = {imt}. Pronounced lack of body weight"
-                )
-
-            elif (imt > 16) and (imt <= 18.5):
-                messagebox.showinfo(
-                    title="Result",
-                    message=f"BMI = {imt}. Body weight deficit"
-                )
-
-            elif (imt > 18.5) and (imt <= 25):
-                messagebox.showinfo(
-                    title="Result",
-                    message=f"BMI = {imt}. Normal weight"
-                )
-
-            elif (imt > 25) and (imt <= 30):
-                messagebox.showinfo(
-                    title="Result",
-                    message=f"BMI = {imt}. Overweight"
-                )
-
-            elif (imt > 30) and (imt <= 35):
-                messagebox.showinfo(
-                    title="Result",
-                    message=f"BMI = {imt}. Obesity of the first degree"
-                )
-
-            elif (imt > 35) and (imt <= 42):
-                messagebox.showinfo(
-                    title="Result",
-                    message=f"BMI = {imt}. Obesity of the second degree"
+                    title="Error",
+                    message="Received a string, but excepted a number"
                 )
 
             else:
-                messagebox.showinfo(
-                    title="Result",
-                    message=f"BMI = {imt}. Obesity of the third degree"
-                )
 
+                imt = m / (h**2)
+                imt = round(imt, 2)
 
-ctk.set_appearance_mode("dark")
-app = ctk.CTk()
-app.title("BMI calculator")
-app.geometry("300x200+650+350")
-app.resizable(width=False, height=False)
+                if (h <= 0) or (m <= 0):
+                    messagebox.showerror(
+                        title="Result",
+                        message="Value is less than zero"
+                    )
 
-height = ctk.CTkLabel(
-    app,
-    text="Enter height: ",
-    text_color="yellow",
-    font=(
-        "Times New Roman",
-        14,
-        "bold"
-    )
-)
+                elif imt <= 16:
+                    messagebox.showinfo(
+                        title="Result",
+                        message=f"BMI = {imt}. Pronounced lack of body weight"
+                    )
 
-height.grid(column=0, row=0)
+                elif (imt > 16) and (imt <= 18.5):
+                    messagebox.showinfo(
+                        title="Result",
+                        message=f"BMI = {imt}. Body weight deficit"
+                    )
 
-mass = ctk.CTkLabel(
-    app,
-    text="Enter mass: ",
-    text_color="yellow",
-    font=(
-        "Times New Roman",
-        14,
-        "bold"
-    )
-)
+                elif (imt > 18.5) and (imt <= 25):
+                    messagebox.showinfo(
+                        title="Result",
+                        message=f"BMI = {imt}. Normal weight"
+                    )
 
-mass.grid(column=0, row=1)
+                elif (imt > 25) and (imt <= 30):
+                    messagebox.showinfo(
+                        title="Result",
+                        message=f"BMI = {imt}. Overweight"
+                    )
 
-tbl_h = ctk.CTkEntry(
-    app,
-    border_width=1,
-    border_color="yellow",
-    width=100
-)
-tbl_h.grid(column=1, row=0, pady=10)
+                elif (imt > 30) and (imt <= 35):
+                    messagebox.showinfo(
+                        title="Result",
+                        message=f"BMI = {imt}. Obesity of the first degree"
+                    )
 
-tbl_m = ctk.CTkEntry(
-    app,
-    border_width=1,
-    border_color="yellow",
-    width=100)
-tbl_m.grid(column=1, row=1, pady=10)
+                elif (imt > 35) and (imt <= 42):
+                    messagebox.showinfo(
+                        title="Result",
+                        message=f"BMI = {imt}. Obesity of the second degree"
+                    )
 
-btn = ctk.CTkButton(
-    app,
-    text="Calculate",
-    text_color="yellow",
-    border_width=1,
-    border_color="yellow",
-    command=calculate_imt
-)
-
-btn.grid(column=1, row=2, pady=10)
+                else:
+                    messagebox.showinfo(
+                        title="Result",
+                        message=f"BMI = {imt}. Obesity of the third degree"
+                    )
 
 
 if __name__ == '__main__':
+    app = Calculator()
     app.mainloop()
